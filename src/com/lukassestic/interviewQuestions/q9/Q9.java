d9b8d6432d4188ce01a6386145e18686c7200f08
Lukas Šestić <lukas.sestic@photomath.net>
2019-11-11 21:53:21 +0100

    Q9 Done

---
+++ src/com/lukassestic/interviewQuestions/q9/Q9.java
@@ -0,0 +1,46 @@
+package com.lukassestic.interviewQuestions.q9;
+
+/**
+ * Check if one string is a rotation of another using only one call to isRotation method.
+ * eg. Word "lukas" has a rotation "ukasl"
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
+public class Q9 {
+
+    private static boolean isRotation(String sub, String of) {
+        int length = of.length();
+        if (sub.length() != length) {
+            return false;
+        }
+       
+        char firstLetter = of.charAt(0);
+        int difference = -1;
+          
+        for(int i = of.length() - 1; i >= 0; i--) {
+            if (sub.charAt(i) == firstLetter) {
+                difference = i;
+                break;
+            }
+        }
+
+        if (difference == -1) {
+            return false;
+        }
+
+        String part1 = sub.substring(0, difference);
+        String part2 = sub.substring(difference, sub.length());
+
+        return of.equals(part2 + part1);
+    }
+
+    public static void main(String[] args){
+        String[] bases = {"lukas", "waterbottle", "bruna"};
+        String[] rotations = {"kaslu", "erbottlewat", "nabrk"};
+
+        for (int i = 0; i < bases.length; i++) {
+            System.out.println(bases[i] + " - " + rotations[i] + " --> " + isRotation(rotations[i], bases[i]));
+        }
+    }
+}
